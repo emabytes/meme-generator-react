1542864ddd9fcba3833d7efd75ce0dccbf545eee
emabytes <62961290+emabytes@users.noreply.github.com>
2021-01-12 18:25:07 +0100

    Initialize project using Create React App

---
+++ MemeGenerator.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class MemeGenerator extends Component {
+    constructor() {
+        super()
+    }
+    
+    state = {}
+    
+    render() { 
+        return ( 
+            <h1>This is meme generator</h1>
+         );
+    }
+}
+ 
+export default MemeGenerator;
